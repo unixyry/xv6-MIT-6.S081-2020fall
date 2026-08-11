01cf503be6c1729d7fe6c92d18258d83d34b251d
unixyry <877324252@qq.com>
2026-08-11 18:28:21 +0800

    [26.08.11][xv6][lab3] - 完成print a page table实验
    [Description]
    通过递归的方式，dfs三级页表中的有效的页表项
    pte & PTE_V == 1 表示页表项有映射
    (pte & (PTE_R | PTE_W | PTE_X)) == 0 表示页表项为根页表和二级页表，需要递归到三级页表

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,31 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+// 打印进程的虚拟内存映射信息
+void vmprint(pagetable_t pagetable, int depth)
+{
+  int i = 0;
+  int j = 0;
+
+  if (depth == 0)
+  {
+    printf("page table %p\n", pagetable);
+    depth++;
+  }
+  
+  for(i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      uint64 child = PTE2PA(pte);
+      printf("..");
+      for (j = 1; j < depth; j++) {
+        printf(" ..");
+      }
+      printf("%d: pte %p pa %p\n", i, pte, child);
+      if((pte & (PTE_R | PTE_W | PTE_X)) == 0){
+        vmprint((pagetable_t)child, depth + 1);
+      }
+    }
+  }
+}
