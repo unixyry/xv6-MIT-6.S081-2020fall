8adb9f977aa8678594c994b944cd8d81824c0145
unixyry <877324252@qq.com>
2026-08-24 22:38:11 +0800

    [26.08.24][xv6][lab7] - 完成 Uthread: switching between threads 实验
    [Description]
    在用户态实现线程切换
    1. 实现uthread_switch，保存旧线程的寄存器(返回地址ra, 线程栈顶指针sp，线程栈帧指针s0，局部变量寄存器s1-s11)，加载新线程的寄存器
    2. 在创建线程时，手动设置ra为thread_a，设置sp为all_thread[i].stack+STACK_SIZE，sp为栈顶指针是从高向低增长，因此手动设置时需要+STACK_SIZE
    3. 不需要保存和恢复其他寄存器, 因为编译器不会在非栈帧区或非s寄存器中保存会被线程调度破坏的局部变量(这与中断和系统调用不同，因为中断可能发生在任意一处，而系统调用则涉及进程切换，所以中断和系统调用需要保存完整的trapframe)

--- user/uthread.c
+++ user/uthread.c
@@ -10,15 +10,33 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct thread_context{
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+}typedef thread_context;
 
 struct thread {
-  char       stack[STACK_SIZE]; /* the thread's stack */
-  int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  thread_context    context; /* 线程上下文 */
+  char              stack[STACK_SIZE];  /* the thread's stack */
+  int               state; /* FREE, RUNNING, RUNNABLE */
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
+extern void thread_switch(thread_context*, thread_context*);
               
 void 
 thread_init(void)
@@ -28,6 +46,7 @@ thread_init(void)
   // save thread 0's state.  thread_schedule() won't run the main thread ever
   // again, because its state is set to RUNNING, and thread_schedule() selects
   // a RUNNABLE thread.
+  memset(&all_thread, 0, sizeof(all_thread));
   current_thread = &all_thread[0];
   current_thread->state = RUNNING;
 }
@@ -63,6 +82,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch(&(t->context), &(next_thread->context));
   } else
     next_thread = 0;
 }
@@ -76,7 +96,8 @@ thread_create(void (*func)())
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
-  // YOUR CODE HERE
+  t->context.ra = (uint64)func;
+  t->context.sp = (uint64)t->stack+STACK_SIZE;
 }
 
 void 
